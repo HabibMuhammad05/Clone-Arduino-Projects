6b9f0868af820666332924e8fd7b3cce52b036b8
Habib Muhammad <101261125+HabibMuhammad05@users.noreply.github.com>
2022-03-27 07:44:03 +0700

    Added Sketch

---
+++ LCD_I2C_CLOCK/LCD_I2C_CLOCK.ino
@@ -0,0 +1,86 @@
+#include <LiquidCrystal_I2C.h>
+#include <TimeLib.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+#define TIME_HEADER  "T"   // Header tag for serial time sync message
+#define TIME_REQUEST  7    // ASCII bell character requests a time sync message 
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin();
+  Serial.begin(9600);
+  while (!Serial) ; // Needed for Leonardo only
+  setSyncProvider( requestSync);  //set function to call when sync required
+  Serial.println("Waiting for sync message");
+  lcd.print("Loading.....");
+  //lcd.print("hello, world!");
+}
+
+void loop() {
+  if (Serial.available()) {
+    processSyncMessage();
+  }
+  if (timeStatus()!= timeNotSet) {
+    digitalClockDisplay();  
+  }
+  if (timeStatus() == timeSet) {
+    digitalWrite(13, HIGH); // LED on if synced
+  } else {
+    digitalWrite(13, LOW);  // LED off if needs refresh
+  }
+  delay(1000);
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 0);
+}
+
+void digitalClockDisplay(){
+  // digital clock display of the time
+  lcd.print("Time ");
+  lcd.print(hour());
+  Serial.print(hour());
+  printDigits(minute());
+  printDigits(second());
+  lcd.setCursor(0, 1);
+  lcd.print("Date ");
+  lcd.print(day());
+  lcd.print("/");
+  lcd.print(month());
+  lcd.print("/");
+  lcd.print(year()); 
+   
+  Serial.print(" ");
+  Serial.print(day());
+  Serial.print(" ");
+  Serial.print(month());
+  Serial.print(" ");
+  Serial.print(year()); 
+  Serial.println(); 
+}
+
+void printDigits(int digits){
+  // utility function for digital clock display: prints preceding colon and leading 0
+  lcd.print(":");
+  if(digits < 10)
+    lcd.print('0');
+  lcd.print(digits);
+}
+
+
+void processSyncMessage() {
+  unsigned long pctime;
+  const unsigned long DEFAULT_TIME = 1357041600; // Jan 1 2013
+
+  if(Serial.find(TIME_HEADER)) {
+     pctime = Serial.parseInt();
+     if( pctime >= DEFAULT_TIME) { // check the integer is a valid time (greater than Jan 1 2013)
+       setTime(pctime); // Sync Arduino clock to the time received on the serial port
+     }
+  }
+}
+
+time_t requestSync()
+{
+  Serial.write(TIME_REQUEST);  
+  return 0; // the time will be sent later in response to serial mesg
+}
